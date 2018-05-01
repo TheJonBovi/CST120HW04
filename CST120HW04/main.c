92835a026fd1695bdcc7fd62feb94a2072990c30
Drew <35358211+TheJonBovi@users.noreply.github.com>
2018-05-01 16:47:53 -0700

    main functions written

---
+++ CST120HW04/main.c
@@ -0,0 +1,258 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>  // NOLINT
+#include <string.h>  // NOLINT
+
+#define OOB 1'000'001
+
+int get_number();
+void check_sign(int);
+void check_parity(int);
+void num_digits(int);
+void print_add_table();
+void print_mul_table();
+
+void main(void)
+{
+	char program_run = 'y';
+	int input_flag = 0;
+	int input = 0;
+	int choice;
+
+	while (program_run == 'y' || program_run == 'Y')
+	{
+		printf("Please Select an option\n\n");
+		printf(" 1 - Enter an Integer\n");
+		printf(" 2 - Sign Check\n");
+		printf(" 3 - Parity Check\n");
+		printf(" 4 - Display Number of Digits\n");
+		printf(" 5 - Display Addition Table\n");
+		printf(" 6 - Display Multiplication Table\n");
+		printf(" 7 - Exit\n\n");
+
+		printf("Enter your choice and press return: ");
+
+		scanf("%d", &choice);
+
+		printf("\n");
+
+		switch (choice)
+		{
+		case 1:
+			input = get_number();
+			input_flag = 1;
+			break;
+
+		case 2:
+			if (input_flag)
+			{
+				check_sign(input);
+			}
+			else
+			{
+				printf("Enter a number first!\n\n");
+			}
+			break;
+
+		case 3:
+			if (input_flag)
+			{
+				check_parity(input);
+			}
+			else
+			{
+				printf("Enter a number first!\n\n");
+			}
+			break;
+
+		case 4:
+			if (input_flag)
+			{
+				num_digits(input);
+			}
+			else
+			{
+				printf("Enter a number first!\n\n");
+			}
+			break;
+
+		case 5:
+			print_add_table();
+			break;
+
+		case 6:
+			print_mul_table();
+			break;
+
+		case 7:
+			printf("End of program.\n\n");
+			program_run = 0;
+			break;
+
+		default:
+			printf("Not a valid choice, choose again.\n\n");
+			printf("Enter your choice and press return: ");
+
+			scanf("%d", &choice);
+			break;
+		}
+	}
+
+	// Below prevents console window from closing after program completion
+	while (getchar() != '\n');
+	printf("press enter to continue...\n");
+	getchar();
+}
+
+// Code help from https://stackoverflow.com/questions/11277641/input-data-validation-in-c
+int get_number()
+{
+	double result = 1'000'001;
+	char input[80]; // arbitrary size buffer
+	printf("Enter a number between -1,000,000 and 1,000,000: ");
+	getchar();
+
+	while (result = OOB)
+	{
+		char* cp = fgets(input, sizeof(input), stdin);
+		if (cp == input)
+		{
+			char* badc; // badc is for detecting extraneous chars in the input
+			int const n = sscanf(input, "%lf %c", &result, &badc);
+			printf("\n");
+
+			if (n != 1) // if badc captured an extraneous char
+			{
+				printf("Error! Please enter an integer: ");
+				result = OOB;
+			}
+			else if (result < -1'000'000 || result > 1'000'000)
+			{
+				printf("Error! Integer out of range, enter a different integer: ");
+				result = OOB;
+			}
+		}
+	}
+	return result;
+}
+
+void check_sign(int const number)
+{
+	if (number >= 0)
+	{
+		printf("The number %d is positive.\n\n", number);
+	}
+	else
+	{
+		printf("The number %d is negative.\n\n", number);
+	}
+}
+
+void check_parity(int const number)
+{
+	if (number % 2 == 0)
+	{
+		printf("The number %d is even.\n\n", number);
+	}
+	else
+	{
+		printf("The number %d is odd.\n\n", number);
+	}
+}
+
+void num_digits(int const number)
+{
+	long n = number;
+	int count = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		++count;
+	}
+
+	printf("The number %d has %d digits.\n\n", number, count);
+}
+
+void print_add_table()
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			if (i + j < 10)
+			{
+				printf(" %d ", i + j);
+			}
+			else
+			{
+				printf("%d ", i + j);
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void print_mul_table()
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			// Special formatting for first row - show the index
+			if (i == 0)
+			{
+				if ( j == 0)
+				{
+					printf(" %d ", j);
+				}
+				else if (j < 10)
+				{
+					printf("  %d ", j);
+				}
+				else
+				{
+					printf(" %d ", j);
+				}
+			}
+			// Special formatting for first column - show the index
+			else if (j == 0)
+			{
+				printf(" %d ", i);
+
+			}
+			// Special formatting for final row due to 3 digit number
+			else if (i == 10)
+			{
+				if ( j <= 1)
+				{
+					printf("%d ", i * j);
+				}
+				else if (j < 10)
+				{
+					printf(" %d ", i * j);
+				}
+				else
+				{
+					printf("%d ", i * j);
+				}
+			}
+			// Fill rest of table with products
+			else if (i * j < 10)
+			{
+				printf("  %d ", i * j);
+			}
+			else if (i * j < 100)
+			{
+				printf(" %d ", i * j);
+			}
+			else
+			{
+				printf("%d ", i * j);
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # CST120HW04
+
+Embedded C Homework 4
+
+---
+
+1.	Write a program that will satisfy the following specifications.
+a.	Display the main menu below.
+ i.	Enter an integer between -1000000 and +1000000. Make sure the user enters a number in range
+ ii.	Is number positive or negative. Provide correct output to monitor depending on input number
+ iii.	Is number odd or even. Provide correct output to monitor depending on input number
+ iv.	Display number of digits. Provide correct output to monitor depending on input number. Example: if number is 534578 then answer is 6
+ v.	Display addition table (square matrix of 0-9). Example:
+	0	1	2	3	4	5	6	7	8	9
+0	0	1	2	3	4	5	6	7	8	9
+1	1	2	3	4	5	6	7	8	9	10
+2	2	3	4	5	6	7	8	9	10	11
+3	3	4	5	6	7	8	9	10	11	12
+4	4	5	6	7	8	9	10	11	12	13
+5	5	6	7	8	9	10	11	12	13	14
+6	6	7	8	9	10	11	12	13	14	15
+7	7	8	9	10	11	12	13	14	15	16
+8	8	9	10	11	12	13	14	15	16	17
+9	9	10	11	12	13	14	15	16	17	18
+
+ vi.	Display multiplication table (square matrix of 0-9)
+ vii.	Exit
+b.	Make sure at least 5 functions are used.
+
